ccdfa4aff0f9930bfc5d497add99790b2b09d162
Van Truong <truongtran.lv2019@gmail.com>
2023-02-28 17:55:00 +0700

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                // "": "",
+                // "": "",
+                // "": "",
+                // "": "",
+                // "": "",
+                // "": "",
+            },
+            fontSize: {
+                12: "12px",
+                14: "14px",
+                16: "16px",
+                20: "20px",
+                24: "24px",
+                26: "26px",
+                32: "32px",
+                36: "36px",
+                48: "48px",
+            },
+        },
+    },
+    plugins: [],
+}

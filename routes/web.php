7a49958c80ddcd1d7b6509a13bbe66d2845bb281
Van Truong <truongtran.lv2019@gmail.com>
2023-03-12 15:09:04 +0700

    update route web

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/', function () {
     return view('homepage');
 });
+
+Route::get('/home', function () {
+    return view('homepage');
+})->name('home');
 
 // auth
 

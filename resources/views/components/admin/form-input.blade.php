ac1635b2c3db2f661a02026cf42852bafa29f820
Van Truong <truongtran.lv2019@gmail.com>
2023-03-12 14:50:52 +0700

    update form-input

--- resources/views/components/admin/form-input.blade.php
+++ resources/views/components/admin/form-input.blade.php
@@ -1,9 +1,14 @@
 <div class="w-[50%] leading-8 pl-4 bg-slate-100 rounded-md border-[1.5px] focus-within:border-[1.5px] focus-within:border-slate-400">
   <form action="" method="post" class="flex justify-between">
     @csrf
-    <input type="text" placeholder="Search..." name="users-search" class="caret-blue-500 outline-none w-full bg-slate-100" required>
-      <button type="submit" class="inline-block w-[18%] cursor-pointer top-0.5">
-        <i class="fa-solid fa-magnifying-glass text-slate-500"></i>
+    <input type="text" placeholder="Search..." name="users-search" class="caret-blue-500 rounded-md outline-none w-full bg-slate-100 relative" required>
+      <button type="submit" class="inline-block top-1.5 absolute right-[340px]">
+        {{-- <i class="fa-solid fa-magnifying-glass text-slate-500"></i> --}}
+        <lord-icon
+          src="https://cdn.lordicon.com/zniqnylq.json"
+          trigger="click"
+          style="width:24px;height:24px">
+      </lord-icon>
       </button>
   </form>
 </div>

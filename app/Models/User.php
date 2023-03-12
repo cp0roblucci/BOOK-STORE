a3a02e96c79b844e5b1323a813c5e482b452f8bd
Van Truong <truongtran.lv2019@gmail.com>
2023-03-13 02:12:23 +0700

    update model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,12 +19,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'cart_id',
         'role_id',
         'first_name',
         'last_name',
         'phone_number',
         'user_address',
+        'google_id',
         'email',
         'password',
         'link_avt',
@@ -48,8 +48,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function products() {
-        return $this->hasMany(Product::class, 'user_id', 'id');
-    }
 }

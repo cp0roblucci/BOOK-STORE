e46b88be84ce981db0c9c5c7a7d8ff501e3d148e
Van Truong <truongtran.lv2019@gmail.com>
2023-03-13 17:09:06 +0700

    update file login.js

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,115 +1,166 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+
+const emailLogin = document.getElementById('email-login');
+const passwordLogin = document.getElementById('password-login');
+
+
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const emailRegister = document.getElementById('email-register');
+const passwordRegister = document.getElementById('password-register');
+const confirmpasswordRegister = document.getElementById('confirm-password-register');
+
+// message error
+//login
+const messageEmailLogin = document.getElementById('message-email-login');
+const messagePasswordLogin = document.getElementById('message-password-login');
+//register
+const messageFirstName = document.getElementById('message-firstname');
+const messageLastName = document.getElementById('message-lastname');
+const messageEmailRegister = document.getElementById('message-email-register');
+const messagePasswordRegister = document.getElementById('message-password-register');
+const messageConfirmPassword = document.getElementById('message-confirm-password');
+
+
+
+// btnsignin & signup
+const btnSiginUp = document.getElementById('sign-up');
+const btnSiginIn = document.getElementById('sign-in');
+
 // show, hidden password login
 
-const passLogin = document.getElementById('passLogin');
 const iconShowPassLogin = document.getElementById('iconShowPassLogin');
 const iconHidePassLogin = document.getElementById('iconHidePassLogin');
 
 
-iconHidePassLogin.addEventListener('click', function() {
-  passLogin.setAttribute('type', 'text');
-  iconShowPassLogin.classList.remove('hidden');
-  iconHidePassLogin.classList.add('hidden');
-});
+if(iconHidePassLogin) {
+  iconHidePassLogin.addEventListener('click', function() {
+    passwordLogin.setAttribute('type', 'text');
+    iconShowPassLogin.classList.remove('hidden');
+    iconHidePassLogin.classList.add('hidden');
+  });
 
-iconShowPassLogin.addEventListener('click', function() {
-  passLogin.setAttribute('type', 'password');
-  iconShowPassLogin.classList.add('hidden');
-  iconHidePassLogin.classList.remove('hidden');
-});
+}
+if(iconShowPassLogin) {
+  iconShowPassLogin.addEventListener('click', function() {
+    passwordLogin.setAttribute('type', 'password');
+    iconShowPassLogin.classList.add('hidden');
+    iconHidePassLogin.classList.remove('hidden');
+  });
+}
 
 // register
-const passRegister = document.getElementById('passRegister');
 const iconShowPassRegister = document.getElementById('iconShowPassRegister');
 const iconHidePassRegister = document.getElementById('iconHidePassRegister');
 
 
-iconHidePassRegister.addEventListener('click', function() {
-  passRegister.setAttribute('type', 'text');
-  iconShowPassRegister.classList.remove('hidden');
-  iconHidePassRegister.classList.add('hidden');
-});
+if(iconHidePassRegister) {
+  iconHidePassRegister.addEventListener('click', function() {
+    passwordRegister.setAttribute('type', 'text');
+    iconShowPassRegister.classList.remove('hidden');
+    iconHidePassRegister.classList.add('hidden');
+  });
+}
 
-iconShowPassRegister.addEventListener('click', function() {
-  passRegister.setAttribute('type', 'password');
-  iconShowPassRegister.classList.add('hidden');
-  iconHidePassRegister.classList.remove('hidden');
-});
+if(iconShowPassRegister) {
+  iconShowPassRegister.addEventListener('click', function() {
+    passwordRegister.setAttribute('type', 'password');
+    iconShowPassRegister.classList.add('hidden');
+    iconHidePassRegister.classList.remove('hidden');
+  });
+}
 
 
  // confirm pass register
-const passConfirmRegister = document.getElementById('confirmPassRegister');
 const iconShowConfirmPass = document.getElementById('iconShowConfirmPass');
 const iconHideConfirmPass = document.getElementById('iconHideConfirmPass');
 
 
-iconHideConfirmPass.addEventListener('click', function() {
-  passConfirmRegister.setAttribute('type', 'text');
-  iconShowConfirmPass.classList.remove('hidden');
-  iconHideConfirmPass.classList.add('hidden');
-});
+if(iconHideConfirmPass) {
+  iconHideConfirmPass.addEventListener('click', function() {
+    confirmpasswordRegister.setAttribute('type', 'text');
+    iconShowConfirmPass.classList.remove('hidden');
+    iconHideConfirmPass.classList.add('hidden');
+  });
 
-iconShowConfirmPass.addEventListener('click', function() {
-  passConfirmRegister.setAttribute('type', 'password');
-  iconShowConfirmPass.classList.add('hidden');
-  iconHideConfirmPass.classList.remove('hidden');
-});
-
-
-// image
-
-const signUp = document.getElementById('signUp');
-const signIn = document.getElementById('signIn');
-const imgLogin1 = document.getElementById('imageLogin1');
-const imgLogin2 = document.getElementById('imageLogin2');
-const formLogin = document.getElementById('formLogin');
-const formRegister = document.getElementById('formRegister');
+}
+if(iconShowConfirmPass) {
+  iconShowConfirmPass.addEventListener('click', function() {
+    confirmpasswordRegister.setAttribute('type', 'password');
+    iconShowConfirmPass.classList.add('hidden');
+    iconHideConfirmPass.classList.remove('hidden');
+  });
+}
 
 
-signUp.addEventListener('click', function() {
-  imgLogin1.classList.add('translate-x-2');
-  imgLogin1.classList.remove('translate-x-[96%]');
 
-  imgLogin1.classList.add('animate-fadeOut');
-  imgLogin1.classList.remove('animate-fadeIn');
+// hidden message error
 
-  imgLogin2.classList.add('z-10');
+// login
 
-  imgLogin2.classList.add('translate-x-2');
-  imgLogin2.classList.remove('translate-x-[96%]');
+if(emailLogin) {
+  emailLogin.addEventListener('input', function() {
+      emailLogin.classList.remove('border-red-500');
+      emailLogin.classList.add('focus-within:border-blue-500');
+      messageEmailLogin.classList.add('hidden');
+  });
+}
 
-  imgLogin2.classList.add('animate-fadeIn');
-  imgLogin2.classList.remove('animate-fadeOut');
+if(passwordLogin) {
+  passwordLogin.addEventListener('input', function() {
+    passwordLogin.classList.remove('border-red-500');
+    passwordLogin.classList.add('focus-within:border-blue-500');
+    messagePasswordLogin.classList.add('hidden');
+  });
+
+}
+
+//register
+
+if(firstName) {
+  firstName.addEventListener('input', function() {
+    firstName.classList.remove('border-red-500');
+    firstName.classList.add('focus-within:border-purple-500');
+    messageFirstName.classList.add('hidden');
+  });
+}
+
+if(lastName) {
+  lastName.addEventListener('input', function() {
+    lastName.classList.remove('border-red-500');
+    lastName.classList.add('focus-within:border-purple-500');
+    messageLastName.classList.add('hidden');
+  });
+}
 
 
-  formLogin.classList.add('animate-fadeOut');
-  formRegister.classList.add('animate-fadeIn');
-
-  formLogin.classList.remove('animate-fadeIn');
-  formRegister.classList.remove('animate-fadeOut');
-});
-
-signIn.addEventListener('click', function() {
-  imgLogin2.classList.remove('translate-x-2');
-  imgLogin2.classList.add('translate-x-[96%]');
-  imgLogin2.classList.add('animate-fadeOut');
-  imgLogin2.classList.remove('animate-fadeIn');
-
-  imgLogin1.classList.remove('translate-x-2');
-  imgLogin1.classList.add('translate-x-[96%]');
-  imgLogin1.classList.add('animate-fadeIn');
-  imgLogin1.classList.remove('animate-fadeOut');
+if(emailRegister) {
+  emailRegister.addEventListener('input', function() {
+    emailRegister.classList.remove('border-red-500');
+    emailRegister.classList.add('focus-within:border-purple-500');
+    messageEmailRegister.classList.add('hidden');
+  });
+}
 
 
-  imgLogin2.classList.remove('z-10');
+if(passwordRegister) {
+  passwordRegister.addEventListener('input', function() {
+    passwordRegister.classList.remove('border-red-500');
+    passwordRegister.classList.add('focus-within:border-purple-500');
+    messagePasswordRegister.classList.add('hidden');
+  });
+}
+
+if(confirmpasswordRegister) {
+  confirmpasswordRegister.addEventListener('input', function() {
+    confirmpasswordRegister.classList.remove('border-red-500');
+    confirmpasswordRegister.classList.add('focus-within:border-purple-500');
+    messageConfirmPassword.classList.add('hidden');
+  });
+
+}
 
 
-  formLogin.classList.add('animate-fadeIn');
-  formRegister.classList.add('animate-fadeOut');
-
-  formLogin.classList.remove('animate-fadeOut');
-  formRegister.classList.remove('animate-fadeIn');
-});
 

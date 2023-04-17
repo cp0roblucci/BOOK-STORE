30b50fe6315bcaea8878687f2926a5b696d98b6e
Van Truong <truongtran.lv2019@gmail.com>
2023-04-17 13:53:14 +0700

    "create new models"

---
+++ app/Models/AccessoriesImport.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AccessoriesImport extends Model
+{
+  protected $table = 'accessories_import_batches';
+  use HasFactory;
+}

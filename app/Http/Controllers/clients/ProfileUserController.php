f510719ece0bb11210f39ba1956df3e805c7c443
hnilney <lhung0099@gmail.com>
2023-04-23 15:58:22 +0700

    action order 1

--- app/Http/Controllers/clients/ProfileUserController.php
+++ app/Http/Controllers/clients/ProfileUserController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class ProfileUserController extends Controller
 {
     public function getprofileUser($user_id) {
+        
         return view('clients.profile_user', compact('user_id'));
     }
 }
